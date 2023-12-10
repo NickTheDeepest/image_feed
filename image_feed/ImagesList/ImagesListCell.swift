118424318bb63db0b50e8221fd3501199f05b426
NickTheDeepest <nsemenov440@gmail.com>
2023-12-10 18:24:07 +0300

    task completed

--- image_feed/ImagesList/ImagesListCell.swift
+++ image_feed/ImagesList/ImagesListCell.swift
@@ -1,16 +1,8 @@
-//
-//  ImagesListCell.swift
-//  image_feed
-//
-//  Created by Никита on 02.12.2023.
-//
-
-import Foundation
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var likeButton: UIButton!
-    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
 }
